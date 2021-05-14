36c1b64e15e14c1e85cbccad3aa9519f1b121c67
Thiago Farias <thiagofarias99@hotmail.com>
2021-05-13 22:35:22 -0300

    organized js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,106 @@
 (function(){
-    let formList = document.querySelector(".form__list_questions");
-    let formQuestions = document.querySelectorAll(".form__question")
-    let checkboxAnswers = document.getElementsByName('question4');
-    var current = 0;
-    let checks = 0
+    Ligthbox()
+    CheckboxConfiguration()
 
-    checkboxAnswers.forEach(e => {
-        e.addEventListener('click', () => {
-            if (e.checked) { 
-                checks++ 
-            } else { 
-                checks-- 
-            }
-            if (checks > 2) {
-                e.checked = false
-                checks--
-            }
+    function CheckboxConfiguration() {
+        let checkboxAnswers = document.getElementsByName('question4');
+        let checks = 0
+
+        checkboxAnswers.forEach(e => {
+            e.addEventListener('click', () => {
+                if (e.checked) { 
+                    checks++ 
+                    checked()
+                } else { 
+                    checks--
+                    if (checks < 1) {
+                        unchecked()
+                    } 
+                }
+                if (checks > 2) {
+                    e.checked = false
+                    checks--
+                    if (checks < 1) {
+                        unchecked()
+                    } 
+                }
+            })
         })
-    })
 
-    let btnPrev = document.querySelector('.form__buttonPrev')
-    let btnNext = document.querySelector('.form__buttonProx')
-    init()
-    function init() {
-        var show = formList.querySelectorAll('.show')
-
-        Array.prototype.forEach.call(show, function(s) {
-            s.classList.remove('show')
-        })
-        formQuestions[0].classList.add('show')
-        btnPrev.removeAttribute('style')
-        btnNext.removeAttribute('style')
-
-        if (formQuestions[0]) {
-            btnPrev.setAttribute('style', 'display: none;')
-        } else {
-            btnPrev.removeAttribute('style')
+        function checked() {
+            checkboxAnswers.forEach(e => {
+                e.removeAttribute('required')
+            })
         }
-
-        addListeners()
-    }
-
-    function addListeners() {
-        btnNext.addEventListener('click', showNext)
-        btnPrev.addEventListener('click', showPrev)
-    }
-
-    function showNext() {
-        current++
-        showSlide()
-    }
-    function showPrev() {
-        current--
-        showSlide()
-    }
-
-    function showSlide() {
-        let qtd = formQuestions.length
-        var slide = current % qtd
-        slide = Math.abs(slide)
-
-        if (formQuestions[slide] == formQuestions[0]) {
-            btnPrev.setAttribute('style', 'display: none;')
-        } else {
-            btnPrev.removeAttribute('style')
+        function unchecked() {
+            checkboxAnswers.forEach(e => {
+                e.setAttribute('required', '')
+            })
         }
-        if (formQuestions[slide] == formQuestions[qtd-=1]) {
-            btnNext.setAttribute('style', 'display: none;')
-        } else {
+    }
+
+    function Ligthbox() {
+        let formList = document.querySelector(".form__list_questions");
+        let formQuestions = document.querySelectorAll(".form__question")
+
+        let btnPrev = document.querySelector('.form__buttonPrev')
+        let btnNext = document.querySelector('.form__buttonProx')
+
+        let current = 0;
+
+        init()
+
+        function init() {
+            var show = formList.querySelectorAll('.show')
+
+            Array.prototype.forEach.call(show, function(s) {
+                s.classList.remove('show')
+            })
+            formQuestions[0].classList.add('show')
+            btnPrev.removeAttribute('style')
             btnNext.removeAttribute('style')
+
+            if (formQuestions[0]) {
+                btnPrev.setAttribute('style', 'display: none;')
+            } else {
+                btnPrev.removeAttribute('style')
+            }
+
+            addListeners()
+
+            function addListeners() {
+                btnNext.addEventListener('click', showNext)
+                btnPrev.addEventListener('click', showPrev)
+            }
+        
+            function showNext() {
+                current++
+                showSlide()
+            }
+            function showPrev() {
+                current--
+                showSlide()
+            }
+        
+            function showSlide() {
+                let qtd = formQuestions.length
+                var slide = current % qtd
+                slide = Math.abs(slide)
+        
+                if (formQuestions[slide] == formQuestions[0]) {
+                    btnPrev.setAttribute('style', 'display: none;')
+                } else {
+                    btnPrev.removeAttribute('style')
+                }
+                if (formQuestions[slide] == formQuestions[qtd-=1]) {
+                    btnNext.setAttribute('style', 'display: none;')
+                } else {
+                    btnNext.removeAttribute('style')
+                }
+                formList.querySelector('.show').classList.remove('show')
+                formQuestions[slide].classList.add('show')
+            }
         }
-        formList.querySelector('.show').classList.remove('show')
-        formQuestions[slide].classList.add('show')
     }
 })()
 
